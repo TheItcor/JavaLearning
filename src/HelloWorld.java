2528d1119e58befa9080305e339b9424b0449d15
TheItcor <Itcorsmail@gmail.com>
2024-10-01 09:39:29 +0300

    Some commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,11 @@
+/**
+ * My first program! Hello, Java!
+ * @author Itcor
+ * @version 1.0
+ */
 public class HelloWorld {
-    public static void main(String[] args){
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); // My first program!
     }
 }
 
---
+++ src/Types.java
@@ -0,0 +1,8 @@
+public class Types {
+    public static void main(String[] args) {
+        // playing with print-format
+        int dob = 51;
+        int delex = dob++ * 3;
+    System.out.printf("progres: %d", delex);
+    }
+}
