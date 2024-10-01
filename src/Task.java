620f4f02a10e464b768d92adc0bad9c681bea9fd
TheItcor <Itcorsmail@gmail.com>
2024-10-01 10:58:30 +0300

    Some commit

--- src/Task.java
+++ src/Task.java
@@ -1,5 +1,11 @@
-public class Task {
+class Main {
     public static void main(String[] args) {
-        
+        String name = "Андрей";
+        int age = 23;
+        double discount = 15.5;
+        System.out.printf("С Днем Рождения, %s! \n", name);
+        System.out.printf("Сегодня Вам %d! \n", age);
+        System.out.printf("По этому поводу Вам скидка %1.1f%% \n", discount);
+        System.out.print("на весь ассортимент нашего сайта!");
     }
 }
