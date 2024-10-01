8151fd5b387c14961c338347d551dbe48a85e2b0
TheItcor <Itcorsmail@gmail.com>
2024-10-01 10:35:38 +0300

    Some commit

--- src/NumberPlusNumber.java
+++ src/NumberPlusNumber.java
@@ -2,10 +2,12 @@ import java.util.Scanner;
 
 public class NumberPlusNumber {
     public static void main(String[] args) {
-        Scanner ScanName = new Scanner(System.in);
-        int NumberX = ScanName.nextInt();
-        int NumberY = ScanName.nextInt();
-        int NumberFinal = NumberX + NumberY;
-        System.out.printf("%d", NumberFinal);
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input first Number: ");
+        int numberX = in.nextInt();
+        System.out.print("Input second Number: ");
+        int numberY = in.nextInt();
+        System.out.printf("Result: %d", numberY+numberX);
+        in.close();
     }
 }
