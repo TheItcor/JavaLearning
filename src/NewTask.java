212f90a576f62bcd410501e327081944ec7b6e5f
itcor <@>
2024-10-05 23:17:11 +0300

    -

---
+++ src/NewTask.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class NewTask {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int numberX = in.nextInt();
+        int numberY = in.nextInt();
+        System.out.println(numberY + numberX);
+    }
+}
--- src/Task.java
+++ src/Task.java
@@ -1,4 +1,4 @@
-class Main {
+class Task {
     public static void main(String[] args) {
         String name = "Андрей";
         int age = 23;
@@ -7,5 +7,6 @@ class Main {
         System.out.printf("Сегодня Вам %d! \n", age);
         System.out.printf("По этому поводу Вам скидка %1.1f%% \n", discount);
         System.out.print("на весь ассортимент нашего сайта!");
+        System.out.println((8 / 4) - (8 + (2 - 1)) + 7 * 2);
     }
 }
