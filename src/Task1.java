6d97cdc906a583dcc2961dcf3de78fe00b1ee489
TheItcor <itcorsmail@gmail.com>
2024-10-07 14:09:04 +0300

    - Some changes for test

--- src/Task1.java
+++ src/Task1.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Task1 {
     public static void main(String[] args) {
         // what's a number is bigger
+        System.out.println("Input a two numbers.");
         Scanner scanner = new Scanner(System.in);
         double number1 = scanner.nextDouble();
         double number2 = scanner.nextDouble();
