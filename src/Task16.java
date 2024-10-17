459e185f41714c0abfccd49fcd9423fb94c841e5
TheItcor <itcorsmail@gmail.com>
2024-10-17 15:55:57 +0300

    - Added some task

---
+++ src/Task16.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Task16 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        var numberA = scanner.nextInt();
+        var numberB = scanner.nextInt();
+        double multiplication = 1;
+        if (numberA > numberB) {
+            int temp;
+            temp = numberB;
+            numberB = numberA;
+            numberA = temp;
+        }
+
+        int i = numberA;
+        while (i <= numberB) {
+            multiplication *= i;
+            i++;
+        }
+
+        System.out.println(multiplication);
+    }
+}
---
+++ src/Task17.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Task17 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        var countNumbers = scanner.nextInt();
+
+
+        int temp = scanner.nextInt();
+        for (int i = 0; i < countNumbers-1; i++) {
+            var number = scanner.nextInt();
+            if (number < temp) {
+                temp = number;
+            }
+        }
+        System.out.println(temp);
+    }
+}
