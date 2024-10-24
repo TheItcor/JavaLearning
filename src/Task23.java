2ddd3387ee10c9bebab1bcaa9a0d83702c8ba300
TheItcor <itcorsmail@gmail.com>
2024-10-24 12:28:45 +0300

    - Added some task

--- src/Task23.java
+++ src/Task23.java
@@ -1,2 +1,9 @@
 public class Task23 {
+    public static void main(String[] args) {
+        String name = "Joe";
+        String wordIs = "is";
+        int age = 256;
+        String finalWorlds = name + " " + wordIs + " " + age;
+        System.out.println(finalWorlds);
+    }
 }
