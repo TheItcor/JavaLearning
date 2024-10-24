616eaf8031b0db3a362803fb363731a7b8e16e13
TheItcor <itcorsmail@gmail.com>
2024-10-24 12:39:37 +0300

    - Added some task

---
+++ src/Task24.java
@@ -0,0 +1,25 @@
+public class Task24 {
+    public static void main(String[] args) {
+        String[] monthsOfYear = new String[12];
+        monthsOfYear[0] = "January";
+        monthsOfYear[1] = "February";
+        monthsOfYear[2] = "March";
+        monthsOfYear[3] = "April";
+        monthsOfYear[4] = "May";
+        monthsOfYear[5] = "June";
+        monthsOfYear[6] = "July";
+        monthsOfYear[7] = "August";
+        monthsOfYear[8] = "September";
+        monthsOfYear[9] = "October";
+        monthsOfYear[10] = "November";
+        monthsOfYear[11] = "December";
+        for (int i = 0; i < 12; i++) {
+            System.out.print(monthsOfYear[i]);
+            if (i == 11) {
+                System.out.print(".");
+            } else {
+                System.out.print(", ");
+            }
+        }
+    }
+}
