70aaad54e28d125c670305c7f442c82f160c0b92
TheItcor <Itcorsmail@gmail.com>
2024-11-04 23:39:56 +0300

    - Test commit

--- src/NewClassesAndTasks/Task37.java
+++ src/NewClassesAndTasks/Task37.java
@@ -2,9 +2,16 @@ package NewClassesAndTasks;
 
 public class Task37 {
     public static void main(String[] args) {
-        //NEW TEST
-        //new test2
-        //NEW TEST3
+        String someString = "Hello! My name is Alex and i'm 29 years old.";
+        String name = someString.substring(18, 22);
+        int age = Integer.parseInt(someString.substring(31, 33));
+        Person personAlex = new Person();
+
+        personAlex.setName(name);
+        personAlex.setAge(age);
+
+        System.out.printf("- His name: %s and he is %d years old", personAlex.getName(), personAlex.getAge());
+
     }
 
 }
